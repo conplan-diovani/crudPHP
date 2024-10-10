142aa21c345e0acf7dcb6d222e98c02df872e168
conplan-diovani <diovani@sgg.net.br>
2024-10-10 10:57:15 -0300

    inicio do projeto

---
+++ configuration/connect.php
@@ -0,0 +1,26 @@
+<?
+    define('HOST', 'localhost');
+    define('DATABASENAME', 'crud-php');
+    define('USER', 'root');
+    define('PASSWORD', '');
+
+    class Connect{
+        protected $connection;
+
+        function __construct()
+        {
+            $this->connectDatabase();
+        }
+
+        function connectDatabase(){
+            try{
+                $this->connection = new PDO('mysql:host='.HOST.';dbname='.DATABASENAME, USER, PASSWORD);
+            }catch(PDOException $e){
+                echo "Error".$e->getMessage();
+                die();
+            }
+        }
+    }
+
+    $testConnection = new Connect();
+?>
